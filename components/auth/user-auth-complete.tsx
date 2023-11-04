9b866072f2061495cf78c33f1ff04baa2eddc8b0
sagorpaul142 <sagorpaulcse@gmail.com>
2023-11-04 12:31:03 +0600

    user-auth-complete code change

--- components/auth/user-auth-complete.tsx
+++ components/auth/user-auth-complete.tsx
@@ -105,7 +105,7 @@ export function UserAuthComplete({uid, callbackUrl, firstName, lastName, email}:
                             placeholder="Last Name"
                             type="text"
                             autoCapitalize="none"
-                            autoComplete="name"
+                            autoComplete="lastName"
                             autoCorrect="off"
                             disabled={isLoading}
                             {...register("lastName")}
