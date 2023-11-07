d7ad90f4a4e49b295e3897bd3a08f89ba0845d36
0xTanvir <tanvir3080@gmail.com>
2023-11-07 23:42:37 +0600

    audit db action added

--- lib/firestore/user.ts
+++ lib/firestore/user.ts
@@ -1,16 +1,18 @@
-import { getDoc, doc, setDoc } from "firebase/firestore"
+import { getDoc, setDoc } from "firebase/firestore"
 import { Collections } from './client'
 import { User } from '@/types/dto'
 
 /// Users ///
 
+// TODO: instead of promise reject throw error
 // getUserById get an user by id
 export async function getUserById(id: string): Promise<User> {
     if (!id) {
         return Promise.reject(Error(`No such user id: ${id}`))
     }
 
-    const snap = await getDoc(doc(Collections.users, id))
+    const userDocRef = Collections.user(id)
+    const snap = await getDoc(userDocRef)
     if (snap.exists()) {
         const data = snap.data()
         const user: User = {
@@ -29,9 +31,9 @@ export async function getUserById(id: string): Promise<User> {
 
 // setUser take UserDto object set it on db
 export async function setUser(id: string, user: User) {
+    const userDocRef = Collections.user(id)
     try {
-        const docRef = doc(Collections.users, id)
-        await setDoc(docRef, user)
+        await setDoc(userDocRef, user)
     } catch (error) {
         return Promise.reject(error)
     }
