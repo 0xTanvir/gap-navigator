5720b9e7d1c1ba3edffd637f9a85287931a1e5b9
sagorpaul142 <sagorpaulcse@gmail.com>
2023-12-05 18:08:49 +0600

    code furnished

--- app/(audit)/audit/[auditId]/[questionId]/answer-list.tsx
+++ app/(audit)/audit/[auditId]/[questionId]/answer-list.tsx
@@ -7,8 +7,6 @@ import { AuditEditorShell } from '../audit-editor-shell'
 import { AuditEditorHeader } from '../audit-editor-header'
 import { Question } from "@/types/dto";
 import { getQuestionById } from "@/lib/firestore/question";
-import { questionSchema } from "@/lib/validations/question";
-import * as z from "zod";
 import AnswerCreateButton from "@/app/(audit)/audit/[auditId]/[questionId]/answer-create-button";
 import AnswerItem from "@/app/(audit)/audit/[auditId]/[questionId]/answer-item";
 import { EmptyPlaceholder } from "@/components/dashboard/empty-placeholder";
