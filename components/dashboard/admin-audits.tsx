42a580c0407d3d768695d44d428d168de21ecbc3
sagorpaul142 <sagorpaulcse@gmail.com>
2023-12-22 18:24:43 +0600

    pagination add .

--- components/dashboard/admin-audits.tsx
+++ components/dashboard/admin-audits.tsx
@@ -1,81 +1,144 @@
 import React, { useEffect, useState } from 'react';
-import { getAudits } from "@/lib/firestore/audit";
-import useAudits from "@/components/dashboard/AuditsContext";
-import { AuditActionType } from "@/types/dto";
-import { toast } from "@/components/ui/use-toast";
+import { fetchAuditsWithCount, fetchPaginatedData } from "@/lib/firestore/audit";
+import { Audits } from "@/types/dto";
 import { DashboardHeader } from "@/components/dashboard/dashboard-header";
 import { AuditCreateButton } from "@/components/dashboard/audit-create-button";
 import { AuditItem } from "@/components/dashboard/audit-item";
 import { EmptyPlaceholder } from "@/components/dashboard/empty-placeholder";
+import Pagination from "@/components/admin/pagination";
 
 interface AdminAuditsProps {
-    userId: string
+  userId: string
 }
 
 const AdminAudits = ({userId}: AdminAuditsProps) => {
-    const [isLoading, setIsLoading] = useState<boolean>(true)
-    const {audits, dispatch} = useAudits()
-
-    useEffect(() => {
-        async function fetchAllAudits() {
-            try {
-                const dbAudits = await getAudits()
-                console.log(dbAudits)
-                dispatch({type: AuditActionType.ADD_MULTIPLE_AUDITS, payload: dbAudits})
-            } catch (err) {
-                toast({
-                    title: "Something went wrong.",
-                    description: `Failed to fetch audits. Please try again, ${err}`,
-                    variant: "destructive",
-                })
-            } finally {
-                setIsLoading(false)
-            }
-        }
-
-        fetchAllAudits()
-    }, [])
-
-    if (isLoading) {
-        return (<>
-                <DashboardHeader heading="Audits" text="Manage audits.">
-                </DashboardHeader>
-                <div className="divide-border-200 divide-y rounded-md border">
-                    <AuditItem.Skeleton/>
-                    <AuditItem.Skeleton/>
-                    <AuditItem.Skeleton/>
-                    <AuditItem.Skeleton/>
-                    <AuditItem.Skeleton/>
-                </div>
-            </>
-        )
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [audit, setAudit] = useState<Audits | []>([])
+  const [pageAction, setPageAction] = useState("NEXT")
+  const [searchName, setSearchName] = useState("")
+  const [page, setPage] = useState(1)
+  const [totalAuditCount, setTotalAuditCount] = useState(0)
+  const [afterThis, setAfterThis] = useState(null)
+  const [beforeThis, setBeforeThis] = useState(null)
+  const PAGE_SIZE: number = 4
+  const fetchData = async () => {
+    const entityObject = {
+      collection: '',
+      records_limit: PAGE_SIZE,
+      pageAction: pageAction,
+      page: page,
+      fields: {
+        // createdAt:true,
+      },
+      orderByField: 'createdAt',
+      orderByOrder: 'asc',
+      last_index: afterThis,
+      first_index: beforeThis,
+      whereFields: [
+        {
+          name: 'name',
+          value: searchName
+        },
+      ]
     }
+    try {
+      const records = await fetchPaginatedData(entityObject)
+      if (records?.length > 0) {
+        const last_index = records.length - 1
+        const first_index = 0
+        // @ts-ignore
+        setAfterThis(records[last_index][entityObject.orderByField])
+        // @ts-ignore
+        setBeforeThis(records[first_index][entityObject.orderByField])
+        setAudit(records)
+      } else {
+        console.log("No data")
+      }
 
-    return (
-        <>
-            <DashboardHeader heading="Audits" text="Manage audits.">
-            </DashboardHeader>
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  const handleNextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+    setPageAction("NEXT")
+  }
+  const handlePrevPage = () => {
+    setPage((prevPage) => prevPage - 1);
+    setPageAction("PREVIOUS")
+  }
 
-            <div>
-                {audits?.length ? (
-                    <div className="divide-y divide-border rounded-md border">
-                        {audits.map((audit) => (
-                            <AuditItem key={audit.uid} userId={userId} audit={audit}/>
-                        ))}
-                    </div>
-                ) : (
-                    <EmptyPlaceholder>
-                        <EmptyPlaceholder.Icon name="audit"/>
-                        <EmptyPlaceholder.Title>No audits created</EmptyPlaceholder.Title>
-                        <EmptyPlaceholder.Description>
-                            You don&apos;t have any audits yet. Start creating audit.
-                        </EmptyPlaceholder.Description>
-                        <AuditCreateButton userId={userId} variant="outline"/>
-                    </EmptyPlaceholder>
-                )}
-            </div>
+  async function auditsCount() {
+    const {totalCount} = await fetchAuditsWithCount(searchName)
+    if (totalCount) {
+      setTotalAuditCount(totalCount)
+    }
+  }
+
+  useEffect(() => {
+    auditsCount()
+  }, [])
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetchData()
+  }, [page])
+
+
+  if (isLoading) {
+    return (<>
+          <DashboardHeader heading="Audits" text="Manage audits.">
+          </DashboardHeader>
+          <div className="divide-border-200 divide-y rounded-md border">
+            <AuditItem.Skeleton/>
+            <AuditItem.Skeleton/>
+            <AuditItem.Skeleton/>
+            <AuditItem.Skeleton/>
+            <AuditItem.Skeleton/>
+          </div>
         </>
-    );
+    )
+  }
+
+  return (
+      <>
+        <DashboardHeader heading="Audits" text="Manage audits.">
+        </DashboardHeader>
+
+        <div>
+          {audit?.length ? (
+              <>
+                <div className="divide-y divide-border rounded-md border">
+                  {audit.map((audit) => (
+                      <AuditItem key={audit.uid} userId={userId} audit={audit}/>
+                  ))}
+                </div>
+                <Pagination
+                    totalAuditCount={totalAuditCount}
+                    page={page}
+                    setPage={setPage}
+                    PAGE_SIZE={PAGE_SIZE}
+                    handlePrevPage={handlePrevPage}
+                    handleNextPage={handleNextPage}
+                />
+              </>
+          ) : (
+              <EmptyPlaceholder>
+                <EmptyPlaceholder.Icon name="audit"/>
+                <EmptyPlaceholder.Title>No audits created</EmptyPlaceholder.Title>
+                <EmptyPlaceholder.Description>
+                  You don&apos;t have any audits yet. Start creating audit.
+                </EmptyPlaceholder.Description>
+                <AuditCreateButton userId={userId} variant="outline"/>
+              </EmptyPlaceholder>
+          )}
+
+
+        </div>
+      </>
+  );
 };
 
 export default AdminAudits;
--- lib/firestore/audit.ts
+++ lib/firestore/audit.ts
@@ -1,137 +1,199 @@
 import {
-    arrayRemove,
-    arrayUnion,
-    deleteDoc,
-    getDoc,
-    getDocs,
-    limit,
-    orderBy,
-    query,
-    setDoc,
-    updateDoc,
-    where
+  arrayRemove,
+  arrayUnion,
+  deleteDoc,
+  getDoc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  setDoc,
+  updateDoc,
+  where, startAfter, endBefore, limitToLast
 } from "firebase/firestore";
 import { Collections } from "./client";
 import { Audit, Audits } from "@/types/dto";
 
 export async function getAuditsByIds(userAuditsId: string[], status: string = ""): Promise<Audits> {
-    if (userAuditsId.length > 0) {
-        const userAuditsCollectionRef = Collections.audits();
-        const q = query(userAuditsCollectionRef, where("uid", "in", userAuditsId));
+  if (userAuditsId.length > 0) {
+    const userAuditsCollectionRef = Collections.audits();
+    const q = query(userAuditsCollectionRef, where("uid", "in", userAuditsId));
 
-        const querySnapshot = await getDocs(q);
+    const querySnapshot = await getDocs(q);
 
-        // If no audits are found, return an empty array instead of rejecting.
-        if (querySnapshot.empty) {
-            return [];
-        }
-        const audits: Audit[] = querySnapshot.docs.map((doc) => {
-            const data = doc.data();
-            return {
-                uid: doc.id,
-                name: data.name,
-                type: data.type,
-                exclusiveList: data.exclusiveList ? data.exclusiveList : [],
-                status: data.status ? data.status : "",
-                authorId: data.authorId,
-                createdAt: data.createdAt,
-            } as Audit;
-        });
-        // Dynamic status filtering
-        const filteredAudits = audits.filter((audit) => audit.status === status)
-        return filteredAudits.sort((a, b) => a.createdAt.seconds - b.createdAt.seconds);
-    } else {
-        return [];
+    // If no audits are found, return an empty array instead of rejecting.
+    if (querySnapshot.empty) {
+      return [];
     }
+    const audits: Audit[] = querySnapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        uid: doc.id,
+        name: data.name,
+        type: data.type,
+        exclusiveList: data.exclusiveList ? data.exclusiveList : [],
+        status: data.status ? data.status : "",
+        authorId: data.authorId,
+        createdAt: data.createdAt,
+      } as Audit;
+    });
+    // Dynamic status filtering
+    const filteredAudits = audits.filter((audit) => audit.status === status)
+    return filteredAudits.sort((a, b) => a.createdAt.seconds - b.createdAt.seconds);
+  } else {
+    return [];
+  }
 }
 
 export async function setAudit(userId: string, audit: Audit): Promise<void> {
-    const newAuditRef = Collections.audit(audit.uid);
-    const createAuditsUser = Collections.userAudits(userId);
-    try {
-        await setDoc(newAuditRef, audit);
-        await updateDoc(createAuditsUser, {
-            audits: arrayUnion(newAuditRef.id),
-        });
-    } catch (error) {
-        // If error is an instance of Error, rethrow it
-        if (error instanceof Error) {
-            throw error;
-        }
-        // If it's not an Error instance, throw a new Error object
-        throw new Error("Failed to add the audit.");
+  const newAuditRef = Collections.audit(audit.uid);
+  const createAuditsUser = Collections.userAudits(userId);
+  try {
+    await setDoc(newAuditRef, audit);
+    await updateDoc(createAuditsUser, {
+      audits: arrayUnion(newAuditRef.id),
+    });
+  } catch (error) {
+    // If error is an instance of Error, rethrow it
+    if (error instanceof Error) {
+      throw error;
     }
+    // If it's not an Error instance, throw a new Error object
+    throw new Error("Failed to add the audit.");
+  }
 }
 
 export async function deleteAudit(userId: string, auditId: string) {
-    const auditRef = Collections.audit(auditId);
-    const userRef = Collections.userAudits(userId);
-    try {
-        await deleteDoc(auditRef);
-        await updateDoc(userRef, {
-            audits: arrayRemove(auditId),
-        });
-    } catch (error) {
-        // Check if the error is an instance of Error and throw it directly if so
-        if (error instanceof Error) {
-            throw error;
-        }
-
-        // Otherwise, throw a new Error with a default or generic message
-        // or convert the unknown error to string if possible
-        throw new Error("An error occurred while deleting the audit.");
+  const auditRef = Collections.audit(auditId);
+  const userRef = Collections.userAudits(userId);
+  try {
+    await deleteDoc(auditRef);
+    await updateDoc(userRef, {
+      audits: arrayRemove(auditId),
+    });
+  } catch (error) {
+    // Check if the error is an instance of Error and throw it directly if so
+    if (error instanceof Error) {
+      throw error;
     }
+
+    // Otherwise, throw a new Error with a default or generic message
+    // or convert the unknown error to string if possible
+    throw new Error("An error occurred while deleting the audit.");
+  }
 }
 
 export async function getAudit(auditId: string) {
-    const result = Collections.audit(auditId);
-    const snap = await getDoc(result);
-    return snap.data() as Audit;
+  const result = Collections.audit(auditId);
+  const snap = await getDoc(result);
+  return snap.data() as Audit;
 }
 
 export async function updateAuditUser(userId: string, auditId: string) {
-    const auditRef = Collections.audit(auditId);
-    const userRef = Collections.userAudits(userId);
-    try {
-        await updateDoc(auditRef, {
-            exclusiveList: arrayRemove(userId),
-        });
-        await updateDoc(userRef, {
-            invitedAuditsList: arrayRemove(auditId),
-        });
-        return true
-    } catch (error) {
-        // Check if the error is an instance of Error and throw it directly if so
-        if (error instanceof Error) {
-            throw error;
-        }
-        // Otherwise, throw a new Error with a default or generic message
-        // or convert the unknown error to string if possible
-        throw new Error("An error occurred while deleting the audit.");
+  const auditRef = Collections.audit(auditId);
+  const userRef = Collections.userAudits(userId);
+  try {
+    await updateDoc(auditRef, {
+      exclusiveList: arrayRemove(userId),
+    });
+    await updateDoc(userRef, {
+      invitedAuditsList: arrayRemove(auditId),
+    });
+    return true
+  } catch (error) {
+    // Check if the error is an instance of Error and throw it directly if so
+    if (error instanceof Error) {
+      throw error;
     }
+    // Otherwise, throw a new Error with a default or generic message
+    // or convert the unknown error to string if possible
+    throw new Error("An error occurred while deleting the audit.");
+  }
 
 }
 
-export async function getAudits(pageSize: number = 10): Promise<Audits> {
-    const auditsCollectionRef = Collections.audits();
-    let q = query(auditsCollectionRef, orderBy("createdAt", 'asc'), limit(pageSize))
+export async function fetchAuditsWithCount(searchName: string): Promise<{ audits: Audits; totalCount: number }> {
+  const auditsCollectionRef = Collections.audits();
+  let queryRef
+  if (searchName) {
+    queryRef = query(auditsCollectionRef, where("name", "==", searchName));
+  } else {
+    queryRef = query(auditsCollectionRef);
+  }
 
-    const querySnapshots = await getDocs(q);
+  const querySnapshots = await getDocs(queryRef);
 
-    // If no audits are found, return an empty array instead of rejecting.
-    if (querySnapshots.empty) {
-        return [];
+  // If no audits are found, return an empty array instead of rejecting.
+  if (querySnapshots.empty) {
+    return {audits: [], totalCount: 0};
+  }
+
+  const audits = querySnapshots.docs.map((doc) => {
+    const data = doc.data();
+    return {
+      uid: doc.id,
+      name: data.name,
+      type: data.type,
+      exclusiveList: data.exclusiveList ? data.exclusiveList : [],
+      status: data.status ? data.status : "",
+      authorId: data.authorId,
+      createdAt: data.createdAt,
+    } as Audit;
+  });
+
+  const totalCount = querySnapshots.size;
+
+  return {audits, totalCount};
+}
+
+
+export async function fetchPaginatedData(entity_object: any) {
+  const {
+    collection, records_limit, pageAction, page, fields, orderByField, orderByOrder,
+    last_index: afterThis, first_index: beforeThis, whereFields
+  } = entity_object
+
+  const collectionRef = Collections.audits()
+  let queryRef = query(collectionRef)
+
+  let searchValue: boolean = false
+  if (whereFields && whereFields.length > 0) {
+    whereFields.forEach((whereObj: any) => {
+      if (whereObj.value) {
+        queryRef = query(queryRef, where(whereObj.name, '==', whereObj.value))
+        searchValue = true
+      }
+    })
+  }
+
+  if (page > 1) {
+    if (pageAction === "NEXT") {
+      queryRef = query(collectionRef, orderBy(orderByField, orderByOrder), startAfter(afterThis), limit(records_limit))
     }
-    return querySnapshots.docs.map((doc) => {
-        const data = doc.data();
-        return {
-            uid: doc.id,
-            name: data.name,
-            type: data.type,
-            exclusiveList: data.exclusiveList ? data.exclusiveList : [],
-            status: data.status ? data.status : "",
-            authorId: data.authorId,
-            createdAt: data.createdAt,
-        } as Audit;
-    });
+    if (pageAction === "PREVIOUS") {
+      queryRef = query(collectionRef, orderBy(orderByField, orderByOrder), endBefore(beforeThis), limitToLast(records_limit))
+    }
+  } else {
+    if (!searchValue) {
+      queryRef = query(collectionRef, orderBy(orderByField, orderByOrder), limit(records_limit))
+      searchValue = false
+    }
+  }
+
+
+  const querySnapshots = await getDocs(queryRef);
+
+  return querySnapshots.docs.map((doc) => {
+    const data = doc.data();
+    return {
+      uid: doc.id,
+      name: data.name,
+      type: data.type,
+      exclusiveList: data.exclusiveList ? data.exclusiveList : [],
+      status: data.status ? data.status : "",
+      authorId: data.authorId,
+      createdAt: data.createdAt,
+    } as Audit;
+  });
+
 }
