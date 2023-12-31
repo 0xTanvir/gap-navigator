0ffbf1a095fdac69b4bada9e7e9f77e5f2e1d4f0
sagorpaul142 <sagorpaulcse@gmail.com>
2023-12-31 21:28:53 +0600

    audit multiple setp form add for audit create and update

--- components/dashboard/audit-create-button.tsx
+++ components/dashboard/audit-create-button.tsx
@@ -11,7 +11,7 @@ import { useForm } from "react-hook-form";
 import { cn } from "@/lib/utils";
 import { Icons } from "@/components/icons";
 import { Audit, AuditActionType } from "@/types/dto";
-import { auditSchema } from "@/lib/validations/audit";
+import { auditSchemaStep1, auditSchemaStep2, auditSchemaStep3 } from "@/lib/validations/audit";
 import { ButtonProps, buttonVariants } from "@/components/ui/button";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
@@ -49,32 +49,58 @@ import dynamic from "next/dynamic";
 const Editor = dynamic(() => import("@/components/editorjs/editor"), {
   ssr: false,
 });
+const EditorData = dynamic(() => import("@/components/editorjs/editor"), {
+  ssr: false,
+});
 
-type FormData = z.infer<typeof auditSchema>;
+type FormDataStep1 = z.infer<typeof auditSchemaStep1>;
+type FormDataStep2 = z.infer<typeof auditSchemaStep2>;
+type FormDataStep3 = z.infer<typeof auditSchemaStep3>;
 
 interface AuditCreateButtonProps extends ButtonProps {
   userId: string;
 }
 
+interface AuditData {
+  auditName: string;
+  auditType: string;
+  welcome: string;
+  thank_you: string;
+}
+
 export function AuditCreateButton({
-  userId,
-  className,
-  variant,
-  ...props
-}: AuditCreateButtonProps) {
-  const { dispatch } = useAudits();
+                                    userId,
+                                    className,
+                                    variant,
+                                    ...props
+                                  }: AuditCreateButtonProps) {
+  const {dispatch} = useAudits();
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const [showAddDialog, setShowAddDialog] = React.useState<boolean>(false);
   const [isTyping, setIsTyping] = useState<boolean>(false);
-  const { user, updateUser } = useAuth();
+  const [current, setCurrent] = useState<number>(0);
+  const [auditFormData, setAuditFormData] = useState<AuditData | null>(null)
+
+  const {user, updateUser} = useAuth();
   const router = useRouter();
 
-  const form = useForm<FormData>({
-    resolver: zodResolver(auditSchema),
+  const formStep1 = useForm<FormDataStep1>({
+    resolver: zodResolver(auditSchemaStep1),
     defaultValues: {
-      auditName: "",
-      auditType: "",
-      description: "",
+      auditName: auditFormData?.auditName || "",
+      auditType: auditFormData?.auditType || "",
+    },
+  });
+  const formStep2 = useForm<FormDataStep2>({
+    resolver: zodResolver(auditSchemaStep2),
+    defaultValues: {
+      welcome: auditFormData?.welcome || "",
+    },
+  });
+  const formStep3 = useForm<FormDataStep3>({
+    resolver: zodResolver(auditSchemaStep3),
+    defaultValues: {
+      thank_you: auditFormData?.thank_you || "",
     },
   });
 
@@ -82,34 +108,65 @@ export function AuditCreateButton({
     setIsTyping(true);
     setTimeout(() => {
       if (!isTyping) {
-        form.trigger("description");
+        formStep2.trigger("welcome");
       }
     }, 1);
 
     if (data.length > 0) {
-      form.setValue("description", JSON.stringify(data));
+      formStep2.setValue("welcome", JSON.stringify(data));
     } else {
-      form.setValue("description", JSON.stringify(undefined));
+      formStep2.setValue("welcome", JSON.stringify(undefined));
+    }
+  };
+  const handleEditorThank_YouData = (data: any) => {
+    setIsTyping(true);
+    setTimeout(() => {
+      if (!isTyping) {
+        formStep3.trigger("thank_you");
+      }
+    }, 1);
+
+    if (data.length > 0) {
+      formStep3.setValue("thank_you", JSON.stringify(data));
+    } else {
+      formStep3.setValue("thank_you", JSON.stringify(undefined));
     }
   };
 
-  async function onSubmit(data: FormData) {
+  const next = () => {
+    setCurrent(prevState => prevState + 1);
+  }
+
+  const prev = () => {
+    setCurrent(prevState => prevState - 1);
+  };
+
+  const onFinishStep1 = (data: FormDataStep1) => {
+    setAuditFormData((prev) => ({...prev as AuditData, ...data}));
+    next()
+  };
+  const onFinishStep2 = (data: FormDataStep2) => {
+    setAuditFormData((prev) => ({...prev as AuditData, ...data}));
+    next()
+  };
+
+  async function onSubmit(data: FormDataStep3) {
     setIsLoading(true);
     try {
       const audit: Audit = {
-        name: data.auditName,
-        type: data.auditType,
-        description: data.description,
+        name: auditFormData?.auditName as string,
+        type: auditFormData?.auditType as string,
+        welcome: auditFormData?.welcome as string,
+        thank_you: data?.thank_you,
         uid: uuidv4(),
         authorId: userId,
         createdAt: Timestamp.now(),
       };
 
       const auditId = await setAudit(userId, audit);
-      dispatch({ type: AuditActionType.ADD_AUDIT, payload: audit });
+      dispatch({type: AuditActionType.ADD_AUDIT, payload: audit});
       user?.audits.push(audit.uid);
       updateUser(user);
-      form.reset();
       router.push(`/audit/${audit.uid}`);
 
       return toast.success("Audit created.", {
@@ -123,120 +180,210 @@ export function AuditCreateButton({
     } finally {
       setIsLoading(false);
       setShowAddDialog(false);
+      formStep1.reset();
+      formStep2.reset();
+      formStep3.reset();
+      setAuditFormData(null)
+      setCurrent(0)
     }
   }
 
   return (
-    <>
-      <Button variant={variant} onClick={() => setShowAddDialog(true)}>
-        <Icons.filePlus className="mr-2 h-4 w-4" />
-        New audit
-      </Button>
+      <>
+        <Button variant={variant} onClick={() => setShowAddDialog(true)}>
+          <Icons.filePlus className="mr-2 h-4 w-4"/>
+          New audit
+        </Button>
 
-      <Sheet open={showAddDialog} onOpenChange={setShowAddDialog}>
-        <SheetContent className="sm:max-w-[50vw] overflow-y-auto">
-          <SheetHeader>
-            <SheetTitle>Add audit</SheetTitle>
-            <SheetDescription>
-              Type audit name, choose an audit type and type audit description.
-            </SheetDescription>
-          </SheetHeader>
+        <Sheet open={showAddDialog} onOpenChange={setShowAddDialog}>
+          <SheetContent className="sm:max-w-[50vw] overflow-y-auto">
+            <SheetHeader>
+              <SheetTitle>Add audit</SheetTitle>
+              <SheetDescription>
+                Type audit name, choose an audit type and type audit description.
+              </SheetDescription>
+            </SheetHeader>
 
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-              <div className="grid gap-4 py-4">
-                <FormField
-                  control={form.control}
-                  name="auditName"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Name</FormLabel>
-                      <FormControl>
-                        <Input
-                          variant="ny"
-                          placeholder="Audit Name"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="auditType"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Type</FormLabel>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select an audit type" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectItem value="private">Private</SelectItem>
-                          <SelectItem value="exclusive">Exclusive</SelectItem>
-                          <SelectItem value="public">Public</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormDescription>
-                        Only public type can be sharable with client. Private
-                        type is only for consultant.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+            {
+              current === 0 ? (
+                      <Form {...formStep1}>
+                        <form onSubmit={formStep1.handleSubmit(onFinishStep1)}>
+                          <div className="grid gap-4 py-4">
+                            <FormField
+                                control={formStep1.control}
+                                name="auditName"
+                                render={({field}) => (
+                                    <FormItem>
+                                      <FormLabel>Name</FormLabel>
+                                      <FormControl>
+                                        <Input
+                                            variant="ny"
+                                            placeholder="Audit Name"
+                                            {...field}
+                                        />
+                                      </FormControl>
+                                      <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={formStep1.control}
+                                name="auditType"
+                                render={({field}) => (
+                                    <FormItem>
+                                      <FormLabel>Type</FormLabel>
+                                      <Select
+                                          onValueChange={field.onChange}
+                                          defaultValue={field.value}
+                                      >
+                                        <FormControl>
+                                          <SelectTrigger>
+                                            <SelectValue placeholder="Select an audit type"/>
+                                          </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                          <SelectItem value="private">Private</SelectItem>
+                                          <SelectItem value="exclusive">Exclusive</SelectItem>
+                                          <SelectItem value="public">Public</SelectItem>
+                                        </SelectContent>
+                                      </Select>
+                                      <FormDescription>
+                                        Only public type can be sharable with client. Private
+                                        type is only for consultant.
+                                      </FormDescription>
+                                      <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                          </div>
 
-                <FormField
-                  control={form.control}
-                  name="description"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Description</FormLabel>
-                      <FormControl>
-                        <Editor
-                          id="description"
-                          onSave={handleEditorSave}
-                          placeHolder="Let`s write description!"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
+                          <SheetFooter>
+                            <button
+                                type="submit"
+                                className={cn(
+                                    buttonVariants({variant: "default"}),
+                                    className
+                                )}
+                                disabled={isLoading}
+                                {...props}
+                            >
+                              Next
+                            </button>
+                          </SheetFooter>
+                        </form>
+                      </Form>
+                  ) :
+                  current === 1 ? (
+                          <Form {...formStep2}>
+                            <form onSubmit={formStep2.handleSubmit(onFinishStep2)}>
+                              <div className="grid gap-4 py-4">
+                                <FormField
+                                    control={formStep2.control}
+                                    name="welcome"
+                                    render={({field}) => (
+                                        <FormItem>
+                                          <FormLabel>Welcome</FormLabel>
+                                          <FormControl>
+                                            <Editor
+                                                id="welcome"
+                                                initialData={
+                                                  auditFormData?.welcome !== undefined
+                                                      ? JSON.parse(auditFormData?.welcome as string)
+                                                      : ""
+                                                }
+                                                onSave={handleEditorSave}
+                                                placeHolder="Let`s write welcome page details!"
+                                            />
+                                          </FormControl>
+                                          <FormMessage/>
+                                        </FormItem>
+                                    )}
+                                />
+                              </div>
 
-              <SheetFooter>
-                <button
-                  type="submit"
-                  // onClick={onClick}
-                  className={cn(
-                    buttonVariants({ variant: "default" }),
-                    {
-                      "cursor-not-allowed opacity-60": isLoading,
-                    },
-                    className
-                  )}
-                  disabled={isLoading}
-                  {...props}
-                >
-                  {isLoading ? (
-                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                  ) : (
-                    <Icons.filePlus className="mr-2 h-4 w-4" />
-                  )}
-                  Submit
-                </button>
-              </SheetFooter>
-            </form>
-          </Form>
-        </SheetContent>
-      </Sheet>
-    </>
+                              <SheetFooter style={{justifyContent: "space-between"}}>
+                                <Button
+                                    onClick={prev}
+                                >
+                                  Prev
+                                </Button>
+
+                                <button
+                                    type="submit"
+                                    className={cn(
+                                        buttonVariants({variant: "default"}),
+                                        className
+                                    )}
+                                    {...props}
+                                >
+                                  Next
+                                </button>
+                              </SheetFooter>
+                            </form>
+                          </Form>
+                      ) :
+                      current === 2 ? (
+                          <Form {...formStep3}>
+                            <form onSubmit={formStep3.handleSubmit(onSubmit)}>
+                              <div className="grid gap-4 py-4">
+                                <FormField
+                                    control={formStep3.control}
+                                    name="thank_you"
+                                    render={({field}) => (
+                                        <FormItem>
+                                          <FormLabel>Thank You</FormLabel>
+                                          <FormControl>
+                                            <EditorData
+                                                id="thank_you"
+                                                initialData={
+                                                  auditFormData?.thank_you !== undefined
+                                                      ? JSON.parse(auditFormData?.thank_you as string)
+                                                      : ""
+                                                }
+                                                onSave={handleEditorThank_YouData}
+                                                placeHolder="Let`s write thank you page details!"
+                                            />
+                                          </FormControl>
+                                          <FormMessage/>
+                                        </FormItem>
+                                    )}
+                                />
+                              </div>
+
+                              <div style={{justifyContent: "space-between", display: 'flex'}}>
+                                <Button
+                                    onClick={prev}
+                                >
+                                  Prev
+                                </Button>
+
+                                <button
+                                    type="submit"
+                                    className={cn(
+                                        buttonVariants({variant: "default"}),
+                                        {
+                                          "cursor-not-allowed opacity-60": isLoading,
+                                        },
+                                        className
+                                    )}
+                                    disabled={isLoading}
+                                    {...props}
+                                >
+                                  {isLoading ? (
+                                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin"/>
+                                  ) : (
+                                      <Icons.filePlus className="mr-2 h-4 w-4"/>
+                                  )}
+                                  Submit
+                                </button>
+                              </div>
+                            </form>
+                          </Form>
+                      ) : null
+            }
+
+          </SheetContent>
+        </Sheet>
+      </>
   );
 }
--- lib/validations/audit.ts
+++ lib/validations/audit.ts
@@ -1,23 +1,49 @@
 import * as z from "zod"
 
 export const auditSchema = z.object({
-    auditName: z
-        .string({
-            required_error: "Please type an audit name.",
-        }).min(1),
-    auditType: z
-        .string({
-            required_error: "Please select an audit type.",
-        }).min(1),
-  description: z
+  auditName: z
       .string({
-        required_error: "Please type an audit description.",
+        required_error: "Please type an audit name.",
+      }).min(1),
+  auditType: z
+      .string({
+        required_error: "Please select an audit type.",
+      }).min(1),
+  welcome: z
+      .string({
+        required_error: "Please type an audit evaluation welcome page.",
+      }).min(1),
+  thank_you: z
+      .string({
+        required_error: "Please type an audit evaluation thank you page.",
+      }).min(1),
+})
+export const auditSchemaStep1 = z.object({
+  auditName: z
+      .string({
+        required_error: "Please type an audit name.",
+      }).min(1),
+  auditType: z
+      .string({
+        required_error: "Please select an audit type.",
+      }).min(1),
+})
+export const auditSchemaStep2 = z.object({
+  welcome: z
+      .string({
+        required_error: "Please type an audit evaluation welcome page.",
+      }).min(1),
+})
+export const auditSchemaStep3 = z.object({
+  thank_you: z
+      .string({
+        required_error: "Please type an audit evaluation thank you page.",
       }).min(1),
 })
 
 export const auditInviteSchema = z.object({
-    email: z.string({required_error: 'Please enter a valid email'}).email('Please enter a valid email')
+  email: z.string({required_error: 'Please enter a valid email'}).email('Please enter a valid email')
 })
 export const auditShareSchema = z.object({
-    email: z.string({required_error: 'Please enter a valid email'}).email('Please enter a valid email')
+  email: z.string({required_error: 'Please enter a valid email'}).email('Please enter a valid email')
 })
