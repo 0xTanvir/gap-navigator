3208c753f0e3ee6dbedd73a46cdcf7d80c54e9d6
sagorpaul142 <sagorpaulcse@gmail.com>
2024-01-02 16:14:27 +0600

    preview and evaluate completed page heading add

--- app/(evaluate)/preview/[auditId]/completed/preview-pdf-download.tsx
+++ app/(evaluate)/preview/[auditId]/completed/preview-pdf-download.tsx
@@ -5,6 +5,7 @@ import pdfFonts from "pdfmake/build/vfs_fonts"
 import edjsParser from "editorjs-parser";
 import { Button } from "@/components/ui/button";
 import usePreview from "@/app/(evaluate)/preview-context";
+import { DocsPageHeader } from "@/app/(evaluate)/preview/page-header";
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs
 
@@ -17,8 +18,12 @@ const PreviewPdfDownload = () => {
   const markup = parser.parse(data);
   return (
       <>
+        <DocsPageHeader
+            heading={preview.name}
+            text={"Analyze your report."}
+        />
         <div className="w-full text-end">
-          <Button disabled>Generate PDF</Button>
+          <Button>Generate PDF</Button>
         </div>
         <div dangerouslySetInnerHTML={{__html: markup}}></div>
       </>
