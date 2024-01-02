733bd19ee20c738e148aaa8f319364b5d155ace9
sagorpaul142 <sagorpaulcse@gmail.com>
2024-01-02 17:45:38 +0600

    notification icon move to main navbar

--- app/(notification)/layout.tsx
+++ app/(notification)/layout.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 import {MainNav} from "@/components/nav/main-nav";
-import {marketingConfig} from "@/config/marketing";
 import {ModeToggle} from "@/components/mode-toggle";
 import {ProfileNav} from "@/components/nav/profile-nav";
 import {SiteFooter} from "@/components/site-footer";
+import NotificationNav from "@/components/nav/notification-nav";
+import { dashboardConfig } from "@/config/dashboard";
 
 interface NotificationLayoutProps {
     children: React.ReactNode
@@ -13,18 +14,19 @@ export default async function NotificationLayout({
     children,
 }: NotificationLayoutProps) {
     return (
-        <div className="flex min-h-screen flex-col">
-            <header className="container z-40 py-6">
-                <div className="flex items-center justify-between">
-                    <MainNav items={marketingConfig.mainNav} />
-                    <nav className="flex gap-2">
-                        <ModeToggle />
-                        <ProfileNav />
-                    </nav>
-                </div>
-            </header>
-            <main className="flex-1">{children}</main>
-            <SiteFooter />
+        <div className="flex min-h-screen flex-col space-y-6">
+          <header className="sticky top-0 z-40 border-b bg-background">
+            <div className="container flex h-20 items-center justify-between py-4">
+              <MainNav items={dashboardConfig.mainNav}/>
+              <nav className="flex gap-2 items-center">
+                <ProfileNav/>
+                <ModeToggle/>
+                <NotificationNav/>
+              </nav>
+            </div>
+          </header>
+          <main className="flex-1">{children}</main>
+          <SiteFooter className="border-t"/>
         </div>
     )
 }
