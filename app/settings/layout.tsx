89d15892488b8f71dac9e578df6a093c3e59be88
sagorpaul142 <sagorpaulcse@gmail.com>
2024-01-24 11:26:22 +0600

    settings page logout issue solved

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -5,6 +5,7 @@ import { MainNav } from "@/components/nav/main-nav";
 import { ModeToggle } from "@/components/mode-toggle";
 import { ProfileNav } from "@/components/nav/profile-nav";
 import { SiteFooter } from "@/components/site-footer";
+import SettingsShell from "@/components/settings/settings-shell";
 
 export const metadata: Metadata = {
   title: "Settings",
@@ -15,26 +16,30 @@ interface SettingsLayoutProps {
   children: React.ReactNode;
 }
 
-export default function SettingsLayout({ children }: SettingsLayoutProps) {
+export default function SettingsLayout({children}: SettingsLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col space-y-6">
       <header className="sticky top-0 z-40 border-b bg-background">
         <div className="container flex h-20 items-center justify-between py-4">
-          <MainNav items={settingsConfig.mainNav} />
+          <MainNav items={settingsConfig.mainNav}/>
           <nav className="flex gap-2">
-            <ProfileNav />
-            <ModeToggle />
+            <ProfileNav/>
+            <ModeToggle/>
           </nav>
         </div>
       </header>
 
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
         <aside className="hidden w-[200px] flex-col md:flex">
-          <SettingsNav items={settingsConfig.sidebarNav} />
+          <SettingsNav items={settingsConfig.sidebarNav}/>
         </aside>
-        <div className="flex w-full flex-1 flex-col">{children}</div>
+        <div className="flex w-full flex-1 flex-col">
+          <SettingsShell>
+            {children}
+          </SettingsShell>
+        </div>
       </div>
-      <SiteFooter className="border-t" />
+      <SiteFooter className="border-t"/>
     </div>
   );
 }
