66bbea212f3184a43c6bd846d3c745b51026fa97
sagorpaul142 <sagorpaulcse@gmail.com>
2024-02-05 11:54:05 +0600

    code furnished

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,9 +1,4 @@
 import React from "react"
-import { MainNav } from "@/components/nav/main-nav";
-import { dashboardConfig } from "@/config/dashboard";
-import { ProfileNav } from "@/components/nav/profile-nav";
-import { ModeToggle } from "@/components/mode-toggle";
-import NotificationComponent from "@/components/marketing/notification-component";
 
 interface MarketingLayoutProps {
   children: React.ReactNode
@@ -14,16 +9,6 @@ export default async function MarketingLayout({
                                               }: MarketingLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="sticky top-0 z-40 border-b bg-background">
-        <div className="container flex h-20 items-center justify-between py-4">
-          <MainNav items={dashboardConfig.mainNav}/>
-          <nav className="flex gap-2 items-center">
-            <ProfileNav/>
-            <ModeToggle/>
-            <NotificationComponent/>
-          </nav>
-        </div>
-      </header>
       <main className="flex-1">{children}</main>
     </div>
   )
