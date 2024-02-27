584b9795691308e8fda6a5a7b14989cdb0198352
sagorpaul142 <sagorpaulcse@gmail.com>
2024-02-27 19:38:56 +0600

    filter add

--- app/(audit)/user/audits/[userId]/evaluations/evaluation-list.tsx
+++ app/(audit)/user/audits/[userId]/evaluations/evaluation-list.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { getUserById } from "@/lib/firestore/user";
 import { getAllEvaluationWithAuditName } from "@/lib/firestore/evaluation";
 import { Evaluate, User } from "@/types/dto";
@@ -7,7 +7,7 @@ import { fetchAuditsWithCount } from "@/lib/firestore/audit";
 import EvaluationItem from "@/app/(audit)/user/audits/[userId]/evaluations/evaluation-item";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
 import { DashboardHeader } from "@/components/dashboard/dashboard-header";
 import { EmptyPlaceholder } from "@/components/dashboard/empty-placeholder";
@@ -17,6 +17,7 @@ import { toast } from "sonner";
 import { Skeleton } from "@/components/ui/skeleton";
 import { AuditItem } from "@/components/dashboard/audit-item";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 interface EvaluationListProps {
   userId: string;
@@ -32,49 +33,99 @@ const EvaluationList = ({userId}: EvaluationListProps) => {
     participantPhone: "",
   });
   const [user, setUser] = useState<User | null>(null)
-  const router = useRouter();
 
-  async function fetchUserEvaluation() {
-    try {
-      const dbUser = await getUserById(userId);
-      setUser(dbUser)
-      const {audits} = await fetchAuditsWithCount();
-      const newEvaluation: any = [];
-      for (const audit of audits) {
-        const evaluations = await getAllEvaluationWithAuditName(audit.uid);
-        const uniqueEvaluations = new Set();
-        evaluations.forEach((evaluation) => {
-          const key = `${evaluation.auditName}-${evaluation.participantEmail}`;
-          if (!uniqueEvaluations.has(key)) {
-            uniqueEvaluations.add(key);
-            newEvaluation.push(evaluation);
-          }
-        });
-      }
-      setEvaluations(
-        newEvaluation.filter(
-          (evaluation: Evaluate) => evaluation.participantEmail === dbUser.email
-        )
-      );
-      newEvaluation.filter((evaluation: Evaluate) => {
-        if (evaluation.participantEmail === dbUser.email) {
-          let result = {
-            participantFirstName: evaluation.participantFirstName,
-            participantLastName: evaluation.participantLastName,
-            participantEmail: evaluation.participantEmail,
-            participantPhone: evaluation.participantPhone
-          }
-          setUserInfo(result)
-        }
-      })
-    } catch (error) {
-      toast.error("Failed to fetch evaluations. Please try again.");
-    } finally {
-      setIsLoading(false);
+  const [auditName, setAuditName] = useState<string>("")
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [currentSliceEvaluations, setCurrentSliceEvaluations] = useState<Evaluate[] | []>([]);
+  const [pageSize] = useState<number>(10)
+  const [totalData, setTotalData] = useState<number>(0)
+
+  const router = useRouter();
+  const inputUserNameRef = useRef<HTMLInputElement>(null);
+
+
+  const debounce = (call: any, delay: number) => {
+    let timer: any
+    return function (...args: any) {
+      // @ts-ignore
+      const context = this
+      if (timer) clearTimeout(timer)
+      timer = setTimeout(() => {
+        timer = null
+        call.apply(context, args)
+      }, delay)
     }
   }
 
+  const handleChange = (e: any) => {
+    setAuditName(e.target.value)
+    setCurrentPage(1)
+  }
+  const inputDebounce = useCallback(debounce(handleChange, 1000), [])
+
+  const handleReset = () => {
+    setCurrentPage(1)
+    setAuditName("")
+    // @ts-ignore
+    inputUserNameRef.current.value = '';
+  }
+
+  const indexOfLastAudit = currentPage * pageSize
+  const indexOfFirstAudit = indexOfLastAudit - pageSize
+
   useEffect(() => {
+    if (auditName) {
+      let filterData = evaluations.filter(evaluation => evaluation?.auditName?.toLowerCase().includes(auditName.toLowerCase()));
+      setTotalData(filterData.length)
+      setCurrentSliceEvaluations(filterData.slice(indexOfFirstAudit, indexOfLastAudit))
+    } else {
+      setTotalData(evaluations.length)
+      setCurrentSliceEvaluations(evaluations.slice(indexOfFirstAudit, indexOfLastAudit))
+    }
+  }, [totalData, auditName, indexOfLastAudit, indexOfFirstAudit, evaluations]);
+
+
+  useEffect(() => {
+    async function fetchUserEvaluation() {
+      try {
+        const dbUser = await getUserById(userId);
+        setUser(dbUser)
+        const {audits} = await fetchAuditsWithCount();
+        const newEvaluation: any = [];
+        for (const audit of audits) {
+          const evaluations = await getAllEvaluationWithAuditName(audit.uid);
+          const uniqueEvaluations = new Set();
+          evaluations.forEach((evaluation) => {
+            const key = `${evaluation.auditName}-${evaluation.participantEmail}`;
+            if (!uniqueEvaluations.has(key)) {
+              uniqueEvaluations.add(key);
+              newEvaluation.push(evaluation);
+            }
+          });
+        }
+        setEvaluations(
+          newEvaluation.filter(
+            (evaluation: Evaluate) => evaluation.participantEmail === dbUser.email
+          )
+        );
+        newEvaluation.filter((evaluation: Evaluate) => {
+          if (evaluation.participantEmail === dbUser.email) {
+            let result = {
+              participantFirstName: evaluation.participantFirstName,
+              participantLastName: evaluation.participantLastName,
+              participantEmail: evaluation.participantEmail,
+              participantPhone: evaluation.participantPhone
+            }
+            setUserInfo(result)
+          }
+        })
+      } catch (error) {
+        toast.error("Failed to fetch evaluations. Please try again.");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
     fetchUserEvaluation();
   }, []);
 
@@ -280,21 +331,40 @@ const EvaluationList = ({userId}: EvaluationListProps) => {
           heading="Evaluations"
           text=""
         ></DashboardHeader>
+
+        <div className="px-1.5 flex flex-col sm:flex-row  sm:items-end justify-end gap-3 mr-1 mb-5">
+          <div className="">
+            <Input
+              placeholder="Aduit Name"
+              ref={inputUserNameRef}
+              type="text"
+              onChange={(e) => {
+                inputDebounce(e)
+              }}
+            />
+          </div>
+          {auditName &&
+              <Button
+                  variant="destructive"
+                  type="reset"
+                  onClick={handleReset}
+              >
+                  <Icons.searchX/>
+              </Button>
+          }
+        </div>
+
         <div>
-          {evaluations?.length ? (
+          {currentSliceEvaluations?.length ? (
             <div className="divide-y divide-border rounded-md border mt-3">
-              {evaluations.map((evaluation, index) => (
+              {currentSliceEvaluations.map((evaluation, index) => (
                 <EvaluationItem key={index} evaluation={evaluation}/>
               ))}
             </div>
           ) : (
-            <EmptyPlaceholder className="mt-3">
-              <EmptyPlaceholder.Icon name="evaluate"/>
-              <EmptyPlaceholder.Title>No evaluations</EmptyPlaceholder.Title>
-              <EmptyPlaceholder.Description>
-                You don&apos;t have any evaluations yet.
-              </EmptyPlaceholder.Description>
-            </EmptyPlaceholder>
+            <div className="divide-y divide-border rounded-md border">
+              <div className="text-center font-semibold py-10">No Data Found</div>
+            </div>
           )}
         </div>
 
